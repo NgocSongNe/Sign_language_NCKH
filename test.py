b8ea256e11506453d8d4bdba4fb49e4cde55fdd8
NgocSongNe <ngocsong309@gmail.com>
2023-12-07 07:57:51 +0700

    .

--- test.py
+++ test.py
@@ -74,13 +74,13 @@ for action in actions:
 
 model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(60,126))) # 64
-model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(256, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.load_weights('13.h5')
+model.load_weights('1325001.h5')
 # 1. New detection variables
 sequence = []
 sentence = []
